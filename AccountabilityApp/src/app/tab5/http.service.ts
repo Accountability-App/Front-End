bcbcd7af41732a188514a142533df951dfb0e26b
Jacob <Jawillhoite@gmail.com>
2021-03-28 14:19:18 -0500

    fixed

--- AccountabilityApp/src/app/tab5/http.service.ts
+++ AccountabilityApp/src/app/tab5/http.service.ts
@@ -7,8 +7,9 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
     constructor(private http: HttpClient) { }
 
+  //TODO: Somehow get this to get data from the logged in user, not just ArchDruid
     getData() {
-        return this.http.get("assets/profileSample.json");
+        return this.http.get("http://localhost:8082/ProfileTab/ArchDruid");
     }
 
     getTaskData() {
