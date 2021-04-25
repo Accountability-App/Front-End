548250efa3cb1c33309f4d8adb54bebdd07628b6
Caleb H <calebhock@mst.edu>
2021-04-25 11:49:03 -0500

    Work on helper page

--- AccountabilityApp/src/app/tab4/tab4.page.ts
+++ AccountabilityApp/src/app/tab4/tab4.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-tab4',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class Tab4Page {
 
-  constructor() {}
+  taskData: Object;
+  profileData: Object;
+
+  constructor(private _http: HttpService) { }
+
+
+  ngOnInit() {
+    this._http.getTaskData().subscribe(t_data => {
+      this.taskData = t_data;
+      console.log(this.taskData);
+    } )
+    this._http.getData().subscribe(p_data => {
+      this.profileData = p_data;
+      console.log(this.profileData);
+    })
+  }
 
 }
