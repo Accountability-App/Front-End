c379d2e170bd53e31dfd3446a0033b2f3d397d23
Caleb H <calebhock@mst.edu>
2021-05-04 17:51:45 -0500

    Continue work on helper page

--- AccountabilityApp/src/app/tab4/tab4.page.ts
+++ AccountabilityApp/src/app/tab4/tab4.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpService } from './http.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab4',
@@ -9,16 +10,23 @@ import { HttpService } from './http.service';
 export class Tab4Page {
 
   taskData: Object;
-  profileData: Object;
+  specificProfileData: Object;
+  myArray: any;
+  length: number;
 
-  constructor(private _http: HttpService) { }
-
+  constructor(private _http: HttpService, private router: Router) { }
+  username: string;
 
   ngOnInit() {
-    this._http.getTaskData().subscribe(t_data => {
-      this.taskData = t_data;
-      console.log(this.taskData);
-    } )
+    this.username = "RoketWarrior";
+    this.grabTaskData();
   }
 
+  async grabTaskData() {
+    this._http.getMyTasks(this.username).subscribe(task_data => {
+      this.taskData = task_data;
+      this.myArray = JSON.stringify(this.taskData);
+      this.length = this.myArray.length;
+    })
+  }
 }
